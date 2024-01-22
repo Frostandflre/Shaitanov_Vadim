a27396db603dee44b91e1e36da2b39bc574a87a4
Frostandflre <mineciler555@gmail.com>
2024-01-22 03:18:09 +0300

    Задание №1

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import openpyxl
-import streamlit as st
-
-st.set_page_config(page_title= "Шайтанов Вадим 2 задание",
-                   page_icon=":monkey:",
-                   layout="wide")
 df = pd.read_excel(
-    io='supermarkt_sales.xlsx',
+    io='ТАКСИ ЗАДАНИЕ 1.xlsx',
     engine='openpyxl',
-    sheet_name='Sales',
-    skiprows=313,
-    usecols='B:R',
-    nrows=200,
+    sheet_name='LOL',
+    skiprows=0,
+    usecols='A:D',
+    nrows=16,
 )
 
-st.dataframe(df)
+print(df)
