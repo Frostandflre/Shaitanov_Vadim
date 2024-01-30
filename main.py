832c936821d358ae4c396643d42d37def66e5b93
Frostandflre <mineciler555@gmail.com>
2024-01-30 16:09:03 +0300

    Задание №8

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import plotly.express as px
 st.set_page_config(page_title="Сайт для мат.моделирования",page_icon = ":chicken:")
 st.title("Графопостроитель")
 st.subheader("дайте мне свой файл Excel")
+
+uploaded_file = st.file_uploader("Выберите файл", type="xlsx")
+if uploaded_file:
+    df = pd.read_excel(uploaded_file, engine="openpyxl")
+    st.dataframe(df)
